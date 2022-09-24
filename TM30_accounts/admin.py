b53c2ba184ad2e287e1adaa626402f33e6a47972
unknown <anthonyolowuxx6@gmail.com>
2022-09-24 13:53:19 +0100

    update

--- TM30_accounts/admin.py
+++ TM30_accounts/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CartAdmin(admin.ModelAdmin):
+    list_display_links = ('username',)
+    list_display = ('username', 'first_name', 'last_name', 'email', 'gender')
+    list_filter = ('username', 'gender', 'is_staff')
+    search_fields = ('username', 'gender', 'is_staff')
+    list_editable = ['email', 'gender']
--- TM30_app/admin.py
+++ TM30_app/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Cart, Item
 
-admin.site.register([Cart, Item])
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_item', 'status')
+    list_filter = ('status', 'user', 'cart_item')
+    search_fields = ('status', 'user', 'cart_item')
+    raw_id_fields = ('user',)
+    list_editable = ['status']
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display_links = ('item_name',)
+    list_display = ('item_name', 'price', 'quantity_available')
+    list_filter = ('item_name', 'quantity_available')
+    search_fields = ('item_name', 'quantity_available')
+    list_editable = ['quantity_available', 'price']
