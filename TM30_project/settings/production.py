18c87901d902a97751c3b2f026d8e600ad90383c
unknown <anthonyolowuxx6@gmail.com>
2022-09-24 01:05:20 +0100

    database updated

--- TM30_project/settings/production.py
+++ TM30_project/settings/production.py
@@ -10,7 +10,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd84bei8buppmau',
         'USER': 'hxtshipeqmkuxq',
-        'PASSWORD': 'password',
+        'PASSWORD': 'f1bb1a5cf0f07a1e6b825dad7fcd040f34ba1930359b74bbda86e223128cb0ad',
         'HOST': 'ec2-52-200-5-135.compute-1.amazonaws.com',
         'PORT': '5432',
     }
